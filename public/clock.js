946b55e12f86c8bf4db996a4b7b52b8c2b6833b8
iaagwl <iaagwl@gmail.com>
2017-06-09 00:27:56 +0200

    add animations

--- public/clock.js
+++ public/clock.js
@@ -2,15 +2,14 @@ const hoursEl = document.getElementById('hours'),
       minutesEl = document.getElementById('minutes'),
       startButton = document.getElementById('start-button'),
       chatContainer = document.getElementById('chat-container'),
-      clockContainer = document.getElementById('clock-container')
+      clockContainer = document.getElementById('clock-container'),
+      clockBody = document.getElementById('clock-body')
 
-let counting = true
 
 function clock(){
   let time = new Date(),
       hours = time.getHours().toString(),
       minutes = time.getMinutes().toString()
-      seconds = time.getSeconds().toString()
 
   if (hours.length < 2) {
     hours = '0' + hours
@@ -25,13 +24,11 @@ function clock(){
 }
 
 startButton.addEventListener('click', () => {
-  clockContainer.classList.add('hide')
+  clockContainer.classList.add('clock-animation')
+  chatContainer.classList.add('chat-animation')
   startButton.classList.add('hide')
-  chatContainer.classList.remove('hide')
-  counting = false
+  clockBody.classList.add('unhide')
 })
 
 clock()
-setInterval(() => {
-  if(counting) clock()
-},1000)
+setInterval(clock, 1000)
